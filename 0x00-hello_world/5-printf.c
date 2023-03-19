4f43af7e71eb421b38e258295313dc2ba684c6c5
kuhlekwethu <kuhlekwethungwenya@gmail.com>
2023-03-19 08:23:32 -0700

    hello world

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,10 +3,10 @@
 /**
  * main - this is a program to print
  *
- * Return 0 when succesful
+ * Return: 0 when succesful
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art");
+	printf("with proper grammar, but the outcome is a piece of art,");
 	return (0);
 }
