d01fd7892455a5bb67ec1bb8eac5502812736e55
kuhlekwethu <kuhlekwethungwenya@gmail.com>
2023-03-20 16:18:14 -0700

    i cant sleep

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,33 +1,17 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main- cant sleep
  *
- * Return: Always 0 (Succes)
+ * Return: always 0
  */
 
 int main(void)
 {
-	int n;
-	int i = 0;
+	char i;
 
-	for (n = 0 ; n < 100 ; n++)
-	{
-		putchar(n / 10 + '0');
-		putchar(i + '0');
-		if (n < 99)
-		{
-			putchar(',');
-			putchar(32);
-		}
-		i++;
-		if (i > 9)
-		{
-			i = 0;
-		}
-	}
+	for (i = 'a' ; i <= 'z' ; i++)
+		putchar(i);
 	putchar('\n');
 	return (0);
 }
-
-
